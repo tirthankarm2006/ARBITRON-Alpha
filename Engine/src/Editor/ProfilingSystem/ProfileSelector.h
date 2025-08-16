9f856af72d455db0c0059d855c724703839ddd7f
tirthankarm2006 <tirthankarm2006@gmail.com>
2025-08-16 23:19:22 +0530

    Changes to Profiling system

---
+++ Engine/src/Editor/ProfilingSystem/ProfileSelector.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "EditorLog.h"
+#include "Profile.h"
+
+namespace ARB {
+	namespace Editor {
+		class ProfileSelector
+		{
+		public:
+			ProfileSelector(std::string profileDataLoc);
+			void runProfileSelector();
+		private:
+			std::shared_ptr<Editor::Log> profilerLogger;
+			std::vector<Profile*> profiles;
+		};
+	}
+}
+
--- Engine/src/Main.cpp
+++ Engine/src/Main.cpp
@@ -1,8 +1,10 @@
 #include "ARBpch.h"
 #include "Engine.h"
-#include "Renderer/Renderer3D.h"
+#include "ProfileSelector.h"
 
 int main() {
+	ARB::Editor::ProfileSelector profileSelector = ARB::Editor::ProfileSelector("ARB_Profiles");
+
 	ARB::Engine ourApp = ARB::Engine(1280, 720, "Arbitrary Editor");
 	ourApp.runEditor();
 	return 0;
